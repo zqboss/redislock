a7dafdc218008115a64120a42388597f122a2adf
zqboss <1026868371@qq.com>
2018-09-20 11:46:05 +0800

    打算

--- src/main/java/com/nicky/redis/RedisUtil.java
+++ src/main/java/com/nicky/redis/RedisUtil.java
@@ -22,7 +22,7 @@ public class RedisUtil {
 
     private static RedisUtil ru = new RedisUtil();
 
-    public static String IP = "192.168.0.205";
+    public static String IP = "10.28.200.248";
 
     public static String PASSWORD = null;
 
@@ -43,7 +43,7 @@ public class RedisUtil {
             // 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
             //config.setMaxIdle(2000);
             // 表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
-           // config.setMaxWaitMillis(1000 * 100);
+            // config.setMaxWaitMillis(1000 * 100);
             //config.setTestOnBorrow(true);
             config.setMaxTotal(200);
             config.setMaxIdle(50);
@@ -51,13 +51,13 @@ public class RedisUtil {
             config.setMaxWaitMillis(10000);
             config.setTestOnBorrow(true);
             config.setTestOnReturn(true);
-//Idle时进行连接扫描
+            //Idle时进行连接扫描
             config.setTestWhileIdle(true);
-//表示idle object evitor两次扫描之间要sleep的毫秒数
+            //表示idle object evitor两次扫描之间要sleep的毫秒数
             config.setTimeBetweenEvictionRunsMillis(30000);
-//表示idle object evitor每次扫描的最多的对象数
+            //表示idle object evitor每次扫描的最多的对象数
             config.setNumTestsPerEvictionRun(10);
-//表示一个对象至少停留在idle状态的最短时间，然后才能被idle object evitor扫描并驱逐；这一项只有在timeBetweenEvictionRunsMillis大于0时才有意义
+            //表示一个对象至少停留在idle状态的最短时间，然后才能被idle object evitor扫描并驱逐；这一项只有在timeBetweenEvictionRunsMillis大于0时才有意义
             config.setMinEvictableIdleTimeMillis(60000);
 
 
--- src/main/java/com/nicky/redislock/RedisFactory.java
+++ src/main/java/com/nicky/redislock/RedisFactory.java
@@ -30,7 +30,7 @@ public class RedisFactory {
 	
 	public static RedisClient getDefaultClient(){
 		JedisPool pool = null;
-		String ip = "192.168.0.205";
+		String ip = "10.28.200.248";
 		int port = 6379;
 		JedisPoolConfig config = new JedisPoolConfig();
 		config.setMaxTotal(200);
@@ -39,13 +39,13 @@ public class RedisFactory {
 		config.setMaxWaitMillis(10000);
 		config.setTestOnBorrow(true);
 		config.setTestOnReturn(true);
-//Idle时进行连接扫描
+		//Idle时进行连接扫描
 		config.setTestWhileIdle(true);
-//表示idle object evitor两次扫描之间要sleep的毫秒数
+		//表示idle object evitor两次扫描之间要sleep的毫秒数
 		config.setTimeBetweenEvictionRunsMillis(30000);
-//表示idle object evitor每次扫描的最多的对象数
+		//表示idle object evitor每次扫描的最多的对象数
 		config.setNumTestsPerEvictionRun(10);
-//表示一个对象至少停留在idle状态的最短时间，然后才能被idle object evitor扫描并驱逐；这一项只有在timeBetweenEvictionRunsMillis大于0时才有意义
+		//表示一个对象至少停留在idle状态的最短时间，然后才能被idle object evitor扫描并驱逐；这一项只有在timeBetweenEvictionRunsMillis大于0时才有意义
 		config.setMinEvictableIdleTimeMillis(60000);
 		pool = new JedisPool(config, ip, port, 100000, RedisUtil.PASSWORD);
 		RedisClient client = new RedisClient(pool);
