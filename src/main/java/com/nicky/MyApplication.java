f2b4433bc0e6511a0765096b08159184576f01b1
zqboss <1026868371@qq.com>
2018-09-21 16:38:52 +0800

    配置文件

--- src/main/java/com/nicky/MyApplication.java
+++ src/main/java/com/nicky/MyApplication.java
@@ -2,9 +2,8 @@ package com.nicky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class MyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
--- src/test/java/com/nicky/redislock/SecKillTest.java
+++ src/test/java/com/nicky/redislock/SecKillTest.java
@@ -26,10 +26,10 @@ public class SecKillTest {
 	private JedisPool jedisPool;
 	@Before
 	public synchronized void  beforeTest() throws IOException{
-		
-		
+
+
 		jedisPool = new JedisPool(RedisUtil.IP);
-		
+
 	}
 
 	@Test
@@ -56,7 +56,7 @@ public class SecKillTest {
 		final CountDownLatch endCount = new CountDownLatch(threadCount);
 		final CountDownLatch beginCount = new CountDownLatch(1);
 		final SecKillImpl testClass = new SecKillImpl();
-		
+
 		Thread[] threads = new Thread[threadCount];
 		//起500个线程，秒杀第一个商品
 		for(int i= 0;i < splitPoint;i++){
@@ -66,7 +66,7 @@ public class SecKillTest {
 						//等待在一个信号量上，挂起
 						beginCount.await();
 						//用动态代理的方式调用secKill方法
-						SeckillInterface proxy = (SeckillInterface) Proxy.newProxyInstance(SeckillInterface.class.getClassLoader(), 
+						SeckillInterface proxy = (SeckillInterface) Proxy.newProxyInstance(SeckillInterface.class.getClassLoader(),
 							new Class[]{SeckillInterface.class}, new CacheLockInterceptor(testClass));
 						proxy.secKill("test", commidityId1);
 						endCount.countDown();
@@ -79,7 +79,7 @@ public class SecKillTest {
 			threads[i].start();
 
 		}
-		
+
 		for(int i= splitPoint;i < threadCount;i++){
 			threads[i] = new Thread(new  Runnable() {
 				public void run() {
@@ -88,7 +88,7 @@ public class SecKillTest {
 						beginCount.await();
 						//用动态代理的方式调用secKill方法
 						beginCount.await();
-						SeckillInterface proxy = (SeckillInterface) Proxy.newProxyInstance(SeckillInterface.class.getClassLoader(), 
+						SeckillInterface proxy = (SeckillInterface) Proxy.newProxyInstance(SeckillInterface.class.getClassLoader(),
 							new Class[]{SeckillInterface.class}, new CacheLockInterceptor(testClass));
 						proxy.secKill("test", commidityId2);
 						//testClass.testFunc("test", 10000001L);
@@ -102,12 +102,12 @@ public class SecKillTest {
 			threads[i].start();
 
 		}
-		
-		
+
+
 		long startTime = System.currentTimeMillis();
 		//主线程释放开始信号量，并等待结束信号量
 		beginCount.countDown();
-		
+
 		try {
 			//主线程等待结束信号量
 			endCount.await();
